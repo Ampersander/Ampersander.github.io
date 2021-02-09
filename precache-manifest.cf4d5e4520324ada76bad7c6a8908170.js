6d77220c863217321843b24a31660fbdfbec3a7f
Ampersander <romain-b49460@hotmail.com>
2021-02-09 13:08:37 +0100

    Updates

---
+++ precache-manifest.cf4d5e4520324ada76bad7c6a8908170.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2d88308703c8ef673977c2ac0b3040",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a26b828c0b084f6ecc36",
+    "url": "/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a26b828c0b084f6ecc36",
+    "url": "/static/js/main.813dc58a.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "16e5325ccee78e7be3e399917e419044",
+    "url": "/static/media/baertromain.16e5325c.jpg"
+  },
+  {
+    "revision": "7470b4db8b55feeef8ed806fa462b39b",
+    "url": "/static/media/resume.7470b4db.pdf"
+  }
+]);
